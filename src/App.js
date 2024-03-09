13069f17f794987fc66a46542b24e7d3799a59fb
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-03-09 16:45:05 +0000

    Register component is logging its result

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import Register from "./Components/Register";
 
 function App() {
+  const handleRegistration = (email, password) => {
+    console.log(email, password);
+  };
   return (
     <div className="App">
       <h1>My portfolio</h1>
-      <Register />
+      <Register onRegister={handleRegistration} />
     </div>
   );
 }
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Form from "./Form";
 
-export default function Register() {
-  const handleFormSubmit = (email, password) => {
-    console.log("Registering with email and password:", email, password);
-  };
-  return <Form onSubmit={handleFormSubmit} />;
+export default function Register({ onRegister }) {
+  return <Form onSubmit={onRegister} />;
 }
