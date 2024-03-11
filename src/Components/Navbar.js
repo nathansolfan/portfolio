c6438071aa115de202a1f1920f527a9f445f0f64
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-03-11 12:46:56 +0000

    Update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link> {/* Link to the Home page */}
+        </li>
+        <li>
+          <Link to="/register">Register</Link> {/* Link to the Register page */}
+        </li>
+        {/* You can add more navigation links here */}
+      </ul>
+    </nav>
+  );
 }
