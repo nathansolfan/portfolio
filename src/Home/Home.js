da5f58ede4a1f0690af9275956f3e03c03f97f9f
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-03-11 21:18:37 +0000

    Background color working correctly, being setup on the home.js component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,20 +2,55 @@ import React, { useState } from "react";
 import "./Home.css";
 
 export default function Home() {
-  const [darkTheme, setDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false); // State to toggle themes
+
+  // Function to toggle the theme
+  const toggleTheme = () => {
+    console.log("Current theme before toggle:", isDarkTheme ? "Dark" : "Light");
+    setIsDarkTheme(!isDarkTheme);
+  };
 
   return (
-    <div className={`home-container ${darkTheme ? "dark-theme" : ""}`}>
-      <h1 className="home-title">Homepage</h1>
-      <p className="home-paragraph">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit...
-      </p>
-      {/* Slider Switch */}
-      <label className="theme-switcher">
-        <input type="checkbox" onChange={() => setDarkTheme(!darkTheme)} />
-        <span className="slider"></span> {/* This line was missing */}
-        Toggle Dark Theme
-      </label>
+    <div>
+      <div
+        className={`home-container ${
+          isDarkTheme ? "dark-theme" : "light-theme"
+        }`}
+      >
+        <style>
+          {`
+        .home-container {
+          background-color: ${isDarkTheme ? "#8fd2ea" : "#d7ab4c"};
+        }
+      `}
+        </style>
+        <h1 className="home-title">Homepage</h1>
+        <p className="home-paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+        </p>
+        <button onClick={toggleTheme}>Toggle Theme</button>{" "}
+        {/* Button to toggle theme */}
+      </div>
+      <div>
+        <p>
+          Eligendi, ipsam tempore nam sunt dolores neque, sequi accusamus saepe
+          porro, nesciunt quibusdam quia unde. Omnis neque reprehenderit nulla
+          laborum quod delectus Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Harum, optio aliquam omnis sit doloremque minus
+          veniam eaque earum, provident modi unde velit incidunt numquam quasi
+          rerum aliquid sequi ipsa repellat.
+        </p>
+      </div>
+      <div>
+        <p>
+          adipisicing elit. Harum, optio aliquam omnis sit doloremque minus
+          veniam eaque earum, provident modi unde velit incidunt numquam quasi
+          Eligendi, ipsam tempore nam sunt dolores neque, sequi accusamus saepe
+          porro, nesciunt quibusdam quia unde. Omnis neque reprehenderit nulla
+          laborum quod delectus Lorem ipsum dolor sit amet consectetur rerum
+          aliquid sequi ipsa repellat.
+        </p>
+      </div>
     </div>
   );
 }
