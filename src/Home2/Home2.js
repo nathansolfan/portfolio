7f4f5ad549e8a3fc033d72689d210e3dc05b45f4
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-03-12 23:52:50 +0000

    Imported BG image

--- src/Home2/Home2.js
+++ src/Home2/Home2.js
@@ -1,5 +1,19 @@
 import React from "react";
+import backgroundImg from "../Images/background.webp";
 
 export default function Home2() {
-  return <div>Home2</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh", // Adjust the height as needed
+      }}
+    >
+      <h2>This is Nathan</h2>
+      <p>I have a strong foundation in front-end and back-end development</p>
+      <button>Learn More</button>
+    </div>
+  );
 }
