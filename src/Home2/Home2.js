61610e952275640c1782bc8cd6cb75cd3c21de6a
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-03-13 00:04:09 +0000

    Updates

--- src/Home2/Home2.js
+++ src/Home2/Home2.js
@@ -1,19 +1,31 @@
 import React from "react";
 import backgroundImg from "../Images/background.webp";
+import "../Home/Home.css";
+import Home from "../Home/HomeColor";
 
 export default function Home2() {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${backgroundImg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "100vh", // Adjust the height as needed
-      }}
-    >
-      <h2>This is Nathan</h2>
-      <p>I have a strong foundation in front-end and back-end development</p>
-      <button>Learn More</button>
+    <div>
+      <div
+        className="hero-section"
+        style={{
+          backgroundImage: `url(${backgroundImg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat", // Prevents the background from repeating
+          backgroundAttachment: "fixed", // Keeps the background stationary
+          overflow: "hidden", // Prevents scrolling due to oversized background
+          height: "100vh", // Adjust the height as needed
+        }}
+      >
+        <h2>This is Nathan</h2>
+        <p>I have a strong foundation in front-end and back-end development</p>
+        <button>Learn More</button>
+      </div>
+
+      <div>
+        <Home />
+      </div>
     </div>
   );
 }
