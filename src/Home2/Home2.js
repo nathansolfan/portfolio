73981fb2ac5557c47009381855e8702b40a6f926
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-03-13 16:02:44 +0000

    More fixes on hero page

--- src/Home2/Home2.js
+++ src/Home2/Home2.js
@@ -1,28 +1,20 @@
 import React from "react";
-import backgroundImg from "../Images/bg.webp";
 import "../Home/Home.css";
 import Home from "../Home/HomeColor";
 
 export default function Home2() {
   return (
     <div>
-      <div
-        className="hero-section"
-        style={{
-          backgroundImage: `url(${backgroundImg})`,
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat", // Prevents the background from repeating
-          backgroundAttachment: "fixed", // Keeps the background stationary
-          overflow: "hidden", // Prevents scrolling due to oversized background
-          height: "100vh", // Adjust the height as needed
-        }}
-      >
-        <h2>This is Nathan</h2>
-        <p>I have a strong foundation in front-end and back-end development</p>
-        <button>Learn More</button>
+      <div className="hero-section">
+        <div className="background-image"></div>
+        <div className="content-overlay">
+          <h2>This is Nathan</h2>
+          <p>
+            I have a strong foundation in front-end and back-end development
+          </p>
+          <button>Learn More</button>
+        </div>
       </div>
-
       <div>
         <Home />
       </div>
