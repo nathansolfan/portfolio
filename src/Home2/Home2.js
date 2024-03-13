fa92aa8442a5b6414a27e729e59ee0054645d981
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-03-13 20:12:24 +0000

    Updated

--- src/Home2/Home2.js
+++ src/Home2/Home2.js
@@ -6,12 +6,9 @@ export default function Home2() {
   return (
     <div>
       <div className="hero-section">
-        <div className="background-image"></div>
         <div className="content-overlay">
           <h2>This is Nathan</h2>
-          <p>
-            I have a strong foundation in front-end and back-end development
-          </p>
+          <p> Proficient in both front-end and back-end development</p>
           <button>Learn More</button>
         </div>
       </div>
