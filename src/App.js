e5bc408a40592340f0683d1461c5f9cdfeff3c8b
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-03-17 19:21:34 +0000

    App.js inserted circle on cursor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home/HomeColor";
+import "./App.css";
 
+import Home from "./Home/HomeColor";
 import Register from "./Components/Register";
 import Navbar from "./Components/Navbar";
 import Home2 from "./Home2/Home2";
@@ -11,11 +12,31 @@ import Logout from "./Components/Logout";
 import Ticktacktoe from "./Components/Ticktacktoe/Ticktacktoe";
 
 function App() {
+  const [cursorPos, setCursorPos] = useState({ x: -100, y: -100 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setCursorPos({ x: event.clientX, y: event.clientY });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <Router>
-      <div>
+      <div className="App">
         <Navbar />{" "}
         {/* Now Navbar and its children have access to routing context */}
+        <div
+          className="cursor-ball"
+          style={{
+            left: `${cursorPos.x}px`,
+            top: `${cursorPos.y}px`,
+          }}
+        ></div>
         <Routes>
           <Route path="/" element={<Home2 />} />
           <Route path="/home" element={<Home />} />
