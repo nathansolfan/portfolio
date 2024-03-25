28796b7ac7e504db909040f050679b2f02c203e5
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-03-25 22:18:36 +0000

    Feedback button hover

--- src/Components/Donate.js
+++ src/Components/Donate.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function Donate() {
   return (
     <div className="donate-container">
+      <p className="title-donate">Please Donate</p>
       <form action="https://www.paypal.com/donate" method="post" target="_top">
         <input type="hidden" name="hosted_button_id" value="U5CGJDRF7F5HQ" />
+
         <input
           type="image"
           src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif"
