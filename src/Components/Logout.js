94e161eca6ae35b467ff317354790416706e947b
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-04-06 15:46:45 +0100

    Update Form component labels and add Logout functionality

--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Logout() {
-  return <div>Logout</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.removeItem("userEmail");
+    navigate("/");
+  }, [navigate]);
+  return null;
 }
--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,6 +1,5 @@
 import React from "react";
-import notfound from "../Images/404.webp";
-
+import notfound from "../Images/4040.webp";
 export default function NotFound() {
   return <img className="notfound" src={notfound} alt="" />;
 }
