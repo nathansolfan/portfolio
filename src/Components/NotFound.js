eebd6ed03a05bfcc4db610ba80412195315ba26b
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-04-06 15:10:51 +0100

    Set 404 picture as background img

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -2,10 +2,5 @@ import React from "react";
 import notfound from "../Images/404.webp";
 
 export default function NotFound() {
-  return (
-    <div className="notfound">
-      <p className="notfound-para">Page not Found</p>
-      <img src={notfound} alt="" />
-    </div>
-  );
+  return <img className="notfound" src={notfound} alt="" />;
 }
