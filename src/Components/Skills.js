ea141c1efd442ccb3590e2e77d7748d777a59032
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-04-09 14:22:48 +0100

    Refactor Skills component and update index.css styles

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -12,25 +12,68 @@ export default function Skills() {
     threshold: 0.1,
     triggerOnce: true,
   });
+  const { ref: ref3, inView: inView3 } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
+  const { ref: ref4, inView: inView4 } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
 
   return (
-    <div className="skills">
-      <div ref={ref1} className={`skills-container ${inView1 ? "reveal" : ""}`}>
-        <p className="skills-para">
-          With over 15 years of hands-on experience, a profound expertise in PC
-          hardware nuances and high-performance computing solutions.
-        </p>
-        <div className="img-box">
-          <img src={pc} alt="PC Logo" />
+    <div>
+      <div className="skills">
+        <div
+          ref={ref1}
+          className={`skills-container ${inView1 ? "reveal" : ""}`}
+        >
+          <p className="skills-para">
+            With over 15 years of hands-on experience, a profound expertise in
+            PC hardware nuances and high-performance computing solutions.
+          </p>
+          <div className="img-box">
+            <img src={pc} alt="PC Logo" />
+          </div>
+        </div>
+        <div
+          ref={ref2}
+          className={`skills-container ${inView2 ? "reveal" : ""}`}
+        >
+          <p className="skills-para">
+            Versatility of Raspberry Pi, specialized in deploying humidity
+            sensors for plant care, ensuring optimal growth and control.
+          </p>
+          <div className="img-box">
+            <img src={raspberry} alt="Raspberry Pi Logo" />
+          </div>
         </div>
       </div>
-      <div ref={ref2} className={`skills-container ${inView2 ? "reveal" : ""}`}>
-        <p className="skills-para">
-          Versatility of Raspberry Pi, specialized in deploying humidity sensors
-          for plant care, ensuring optimal growth and control.
-        </p>
-        <div className="img-box">
-          <img src={raspberry} alt="Raspberry Pi Logo" />
+
+      <div className="skills">
+        <div
+          ref={ref3}
+          className={`skills-container ${inView3 ? "reveal" : ""}`}
+        >
+          <p className="skills-para">
+            With over 15 years of hands-on experience, a profound expertise in
+            PC hardware nuances and high-performance computing solutions.
+          </p>
+          <div className="img-box">
+            <img src={pc} alt="PC Logo" />
+          </div>
+        </div>
+        <div
+          ref={ref4}
+          className={`skills-container ${inView4 ? "reveal" : ""}`}
+        >
+          <p className="skills-para">
+            Versatility of Raspberry Pi, specialized in deploying humidity
+            sensors for plant care, ensuring optimal growth and control.
+          </p>
+          <div className="img-box">
+            <img src={raspberry} alt="Raspberry Pi Logo" />
+          </div>
         </div>
       </div>
     </div>
